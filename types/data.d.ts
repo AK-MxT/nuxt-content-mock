16dbedcd4e704ea6936c6020f52656294a25abd2
Asuka Kajiwara <asuka.kajiwara@tabian.co>
2021-12-20 01:55:55 +0900

    🏷️型定義ファイル追加

---
+++ types/data.d.ts
@@ -0,0 +1,15 @@
+export interface TableData {
+  id: number,
+  title: string,
+  author: string,
+  supervision: string,
+  publisher: string,
+  price: number,
+  genre: string,
+  description: string,
+  releaseDate: Date,
+  purchaseDate: Date,
+  remarks: string,
+  createDate: Date,
+  updateDate: Date
+}
