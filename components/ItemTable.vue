42cb6c4728a188b5c97457e5ef6270413b8202b0
Asuka Kajiwara <asuka.kajiwara@tabian.co>
2021-12-20 01:58:42 +0900

    🚚リネームして更新

---
+++ components/ItemTable.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-card>
+    <v-card-title>
+      書籍一覧
+      <v-spacer />
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        dense
+        label="検索"
+        hide-details
+        outlined
+      />
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="items"
+      :search="search"
+    />
+  </v-card>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, useAsync, useContext } from '@nuxtjs/composition-api'
+import type { TableData } from '~/types/data'
+
+export default defineComponent({
+  setup () {
+    const {
+      $content
+    } = useContext()
+
+    const search = ref('')
+    const headers = [
+      {
+        text: 'タイトル',
+        value: 'title'
+      },
+      {
+        text: '著者',
+        value: 'author'
+      },
+      {
+        text: '出版社',
+        value: 'publisher'
+      },
+      {
+        text: '価格',
+        value: 'price'
+      },
+      {
+        text: '購入日',
+        value: 'purchaseDate'
+      }
+    ]
+
+    const data = ref<any>([])
+    const items = ref<TableData>()
+
+    useAsync(async () => {
+      const query = await $content('data').only(['body'])
+      data.value = await query.fetch()
+      items.value = data.value.body
+    })
+
+    return {
+      headers,
+      items,
+      search
+    }
+  }
+})
+</script>
