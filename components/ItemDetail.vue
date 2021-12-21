fc9964b5dc0a018ac0c6abb667079baf210a2ce9
A.K <ak4.greenworks@gmail.com>
2021-12-22 01:58:19 +0900

    🚧詳細画面実装中

--- components/ItemDetail.vue
+++ components/ItemDetail.vue
@@ -1,3 +1,39 @@
 <template>
   <v-container />
 </template>
+
+<script lang="ts">
+import { defineComponent, ref, useAsync, useContext, useRoute, useRouter } from '@nuxtjs/composition-api'
+import type { TableData } from '~/types/data'
+
+export default defineComponent({
+  setup () {
+    const {
+      $content
+    } = useContext()
+    const route = useRoute()
+    const router = useRouter()
+
+    const data = ref<any>([])
+    const item = ref<TableData>()
+
+    useAsync(async () => {
+      const query = await $content('data').only(['body']).where({ id: route.value.params.id })
+      data.value = await query.fetch()
+      item.value = data.value.body
+    })
+
+    console.log(item.value)
+    console.log(route.value.params.id)
+
+    const toDetail = (id: string) => {
+      router.push(`/${id}`)
+    }
+
+    return {
+      item,
+      toDetail
+    }
+  }
+})
+</script>
