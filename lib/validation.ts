7c2c2d3d3683a7ae1234a9e46c0eaaf0da5c5970
A.K <ak4.greenworks@gmail.com>
2021-12-25 02:03:25 +0900

    ♻️数値のバリデーションを追加

--- lib/validation.ts
+++ lib/validation.ts
@@ -1,3 +1,7 @@
 export function required (val: string): boolean | string {
   return !!val || '入力必須項目です'
 }
+
+export function number (val: number): boolean | string {
+  return !isNaN(val) || '数値を入力してください'
+}
