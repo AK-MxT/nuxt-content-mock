31a5dba29dc03df91743be17b9e0fce2d1bd94fe
A.K <ak4.greenworks@gmail.com>
2021-12-26 01:17:40 +0900

    ♻️詳細画面のフォーム部分を作成中

--- components/ItemDetail.vue
+++ components/ItemDetail.vue
@@ -12,25 +12,25 @@
         v-model="selectedItem.title"
         label="タイトル"
         outlined
-        :readonly="canEdit"
+        :readonly="!canEdit"
       />
       <v-text-field
         v-model="selectedItem.author"
         label="著者"
         outlined
-        :readonly="canEdit"
+        :readonly="!canEdit"
       />
       <v-text-field
         v-model="selectedItem.supervision"
         label="監修"
         outlined
-        :readonly="canEdit"
+        :readonly="!canEdit"
       />
       <v-text-field
         v-model="selectedItem.publisher"
         label="出版社"
         outlined
-        :readonly="canEdit"
+        :readonly="!canEdit"
       />
       <v-text-field
         v-model="selectedItem.price"
@@ -38,14 +38,104 @@
         label="価格"
         outlined
         :rules="[rules.required, rules.number]"
-        :readonly="canEdit"
+        :readonly="!canEdit"
       />
       <v-text-field
         v-model="selectedItem.genre"
         label="ジャンル"
         outlined
-        :readonly="canEdit"
+        :readonly="!canEdit"
       />
+      <v-textarea
+        v-model="selectedItem.description"
+        label="説明"
+        outlined
+        :readonly="!canEdit"
+      />
+      <v-menu
+        v-model="releaseDateMenu"
+        :close-on-content-click="false"
+        :disabled="!canEdit"
+        :nudge-right="40"
+        transition="scale-transition"
+        offset-y
+        min-width="auto"
+      >
+        <template #activator="{ on, attrs }">
+          <v-text-field
+            v-model="selectedItem.releaseDate"
+            label="発売日"
+            outlined
+            prepend-inner-icon="mdi-calendar"
+            readonly
+            v-bind="attrs"
+            v-on="on"
+          />
+        </template>
+        <v-date-picker
+          v-model="selectedItem.releaseDate"
+          :readonly="!canEdit"
+          @input="menu = false"
+        />
+      </v-menu>
+      <v-menu
+        v-model="purchaseDateMenu"
+        :close-on-content-click="false"
+        :disabled="!canEdit"
+        :nudge-right="40"
+        transition="scale-transition"
+        offset-y
+        min-width="auto"
+      >
+        <template #activator="{ on, attrs }">
+          <v-text-field
+            v-model="selectedItem.purchaseDate"
+            label="購入日"
+            outlined
+            prepend-inner-icon="mdi-calendar"
+            readonly
+            v-bind="attrs"
+            v-on="on"
+          />
+        </template>
+        <v-date-picker
+          v-model="selectedItem.purchaseDate"
+          :readonly="!canEdit"
+          @input="menu = false"
+        />
+      </v-menu>
+      <v-textarea
+        v-model="selectedItem.remarks"
+        label="備考"
+        outlined
+        :readonly="!canEdit"
+      />
+      <v-text-field
+        v-model="selectedItem.createDate"
+        disabled
+        label="作成日"
+        outlined
+      />
+      <v-text-field
+        v-model="selectedItem.updateDate"
+        disabled
+        label="更新日"
+        outlined
+      />
+      <v-btn
+        class="mb-4"
+        color="primary"
+        @click="canEdit = true"
+      >
+        編集する
+      </v-btn>
+      <v-btn
+        class="mb-4"
+        color="secondary"
+        @click="toList"
+      >
+        一覧画面へ戻る
+      </v-btn>
     </v-form>
   </v-card>
 </template>
@@ -66,7 +156,9 @@ export default defineComponent({
       required
     }
 
-    const canEdit = ref(true)
+    const canEdit = ref(false)
+    const releaseDateMenu = ref(false)
+    const purchaseDateMenu = ref(false)
 
     const selectedItem = reactive<TableData>({
       id: accessor.selectedItem.id,
@@ -88,11 +180,18 @@ export default defineComponent({
       router.push('/')
     }
 
+    const toList = () => {
+      router.push('/')
+    }
+
     return {
       canEdit,
+      purchaseDateMenu,
+      releaseDateMenu,
       rules,
       save,
-      selectedItem
+      selectedItem,
+      toList
     }
   }
 })
