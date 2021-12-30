c2d1795e9b83085941d455544178540607ebef1a
A.K <ak4.greenworks@gmail.com>
2021-12-30 14:49:49 +0900

    ✨共通コンポーネント作成

---
+++ components/base/form/TextField.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-text-field
+    dense
+    :disabled="disabled"
+    :label="label"
+    outlined
+    :readonly="!canEdit"
+    :rules="rules"
+    :suffix="suffix"
+    :value="value"
+    @input="$emit('input', $event)"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  props: {
+    canEdit: Boolean,
+    disabled: Boolean,
+
+    label: {
+      type: String,
+      default: undefined
+    },
+
+    rules: {
+      type: Array,
+      default: () => []
+    },
+
+    suffix: {
+      type: String,
+      default: undefined
+    },
+
+    value: {
+      type: String,
+      default: undefined
+    }
+  }
+})
+</script>
---
+++ components/base/form/Textarea.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-textarea
+    dense
+    :disabled="disabled"
+    :label="label"
+    outlined
+    :readonly="!canEdit"
+    :rules="rules"
+    :value="value"
+    @input="$emit('input', $event)"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  props: {
+    canEdit: Boolean,
+    disabled: Boolean,
+
+    label: {
+      type: String,
+      default: undefined
+    },
+
+    rules: {
+      type: Array,
+      default: () => []
+    },
+
+    value: {
+      type: String,
+      default: undefined
+    }
+  }
+})
+</script>
