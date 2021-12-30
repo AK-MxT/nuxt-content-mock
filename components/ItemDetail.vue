738ab460eb5ccd86805dd72222c7a8afdcac5051
A.K <ak4.greenworks@gmail.com>
2021-12-30 15:03:10 +0900

    ♻️2列構成にする・v-formを動くようにする・保存処理は作成中

--- components/ItemDetail.vue
+++ components/ItemDetail.vue
@@ -2,171 +2,173 @@
   <v-card
     class="px-4"
   >
-    <v-card-title>
+    <v-card-title
+      class="px-0"
+    >
       書籍詳細情報
     </v-card-title>
     <v-form
       ref="form"
+      v-model="valid"
+      lazy-validation
+      @submit.prevent
     >
-      <v-text-field
-        v-model="selectedItem.title"
-        dense
-        label="タイトル"
-        outlined
-        :readonly="!canEdit"
-      />
-      <v-text-field
-        v-model="selectedItem.author"
-        dense
-        label="著者"
-        outlined
-        :readonly="!canEdit"
-      />
-      <v-text-field
-        v-model="selectedItem.supervision"
-        dense
-        label="監修"
-        outlined
-        :readonly="!canEdit"
-      />
-      <v-text-field
-        v-model="selectedItem.publisher"
-        dense
-        label="出版社"
-        outlined
-        :readonly="!canEdit"
-      />
-      <v-text-field
-        v-model="selectedItem.price"
-        dense
-        suffix="円"
-        label="価格"
-        outlined
-        :rules="[rules.required, rules.number]"
-        :readonly="!canEdit"
-      />
-      <v-text-field
-        v-model="selectedItem.genre"
-        dense
-        label="ジャンル"
-        outlined
-        :readonly="!canEdit"
-      />
-      <v-textarea
-        v-model="selectedItem.description"
-        dense
-        label="説明"
-        outlined
-        :readonly="!canEdit"
-      />
-      <v-menu
-        v-model="releaseDateMenu"
-        :close-on-content-click="false"
-        :disabled="!canEdit"
-        :nudge-right="40"
-        transition="scale-transition"
-        offset-y
-        min-width="auto"
-      >
-        <template #activator="{ on, attrs }">
-          <v-text-field
-            v-model="selectedItem.releaseDate"
-            dense
-            label="発売日"
-            outlined
-            prepend-inner-icon="mdi-calendar"
-            readonly
-            v-bind="attrs"
-            v-on="on"
-          />
-        </template>
-        <v-date-picker
-          v-model="selectedItem.releaseDate"
-          :readonly="!canEdit"
-          @input="menu = false"
-        />
-      </v-menu>
-      <v-menu
-        v-model="purchaseDateMenu"
-        :close-on-content-click="false"
-        :disabled="!canEdit"
-        :nudge-right="40"
-        transition="scale-transition"
-        offset-y
-        min-width="auto"
-      >
-        <template #activator="{ on, attrs }">
-          <v-text-field
-            v-model="selectedItem.purchaseDate"
-            dense
-            label="購入日"
-            outlined
-            prepend-inner-icon="mdi-calendar"
-            readonly
-            v-bind="attrs"
-            v-on="on"
-          />
-        </template>
-        <v-date-picker
-          v-model="selectedItem.purchaseDate"
-          :readonly="!canEdit"
-          @input="menu = false"
-        />
-      </v-menu>
-      <v-textarea
-        v-model="selectedItem.remarks"
-        dense
-        label="備考"
-        outlined
-        :readonly="!canEdit"
-      />
-      <v-text-field
-        v-model="selectedItem.createDate"
-        dense
-        disabled
-        label="作成日"
-        outlined
-      />
-      <v-text-field
-        v-model="selectedItem.updateDate"
-        dense
-        disabled
-        label="更新日"
-        outlined
-      />
-      <v-btn
-        v-if="!canEdit"
-        class="mb-4 mr-2"
-        color="primary"
-        @click="canEdit = true"
-      >
-        編集する
-      </v-btn>
-      <template
-        v-if="canEdit"
-      >
-        <v-btn
-          class="mb-4 mr-2"
-          color="accent"
-          @click="cancel"
+      <v-row>
+        <v-col
+          cols="12"
+          md="6"
         >
-          キャンセル
-        </v-btn>
-        <v-btn
-          class="mb-4 mr-2"
-          color="info"
-          @click="save"
+          <base-form-text-field
+            v-model="selectedItem.title"
+            :can-edit="canEdit"
+            label="タイトル"
+            :rules="[rules.required]"
+          />
+          <base-form-text-field
+            v-model="selectedItem.author"
+            :can-edit="canEdit"
+            label="著者"
+            :rules="[rules.required]"
+          />
+          <base-form-text-field
+            v-model="selectedItem.supervision"
+            :can-edit="canEdit"
+            label="監修"
+          />
+          <base-form-text-field
+            v-model="selectedItem.publisher"
+            :can-edit="canEdit"
+            label="出版社"
+            :rules="[rules.required]"
+          />
+          <base-form-text-field
+            v-model="selectedItem.price"
+            :can-edit="canEdit"
+            suffix="円"
+            label="価格"
+            :rules="[rules.required, rules.number]"
+          />
+          <base-form-text-field
+            v-model="selectedItem.genre"
+            :can-edit="canEdit"
+            label="ジャンル"
+            :rules="[rules.required]"
+          />
+          <base-form-textarea
+            v-model="selectedItem.description"
+            :can-edit="canEdit"
+            label="説明"
+          />
+        </v-col>
+        <v-col
+          cols="12"
+          md="6"
         >
-          更新
-        </v-btn>
-      </template>
-      <v-btn
-        class="mb-4"
-        color="secondary"
-        @click="toList"
-      >
-        一覧画面へ戻る
-      </v-btn>
+          <v-menu
+            v-model="releaseDateMenu"
+            :close-on-content-click="false"
+            :disabled="!canEdit"
+            :nudge-right="40"
+            transition="scale-transition"
+            offset-y
+            min-width="auto"
+          >
+            <template #activator="{ on, attrs }">
+              <v-text-field
+                v-model="selectedItem.releaseDate"
+                dense
+                label="発売日"
+                outlined
+                prepend-inner-icon="mdi-calendar"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+              />
+            </template>
+            <v-date-picker
+              v-model="selectedItem.releaseDate"
+              :readonly="!canEdit"
+              @input="menu = false"
+            />
+          </v-menu>
+          <v-menu
+            v-model="purchaseDateMenu"
+            :close-on-content-click="false"
+            :disabled="!canEdit"
+            :nudge-right="40"
+            transition="scale-transition"
+            offset-y
+            min-width="auto"
+          >
+            <template #activator="{ on, attrs }">
+              <v-text-field
+                v-model="selectedItem.purchaseDate"
+                dense
+                label="購入日"
+                outlined
+                prepend-inner-icon="mdi-calendar"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+              />
+            </template>
+            <v-date-picker
+              v-model="selectedItem.purchaseDate"
+              :readonly="!canEdit"
+              @input="menu = false"
+            />
+          </v-menu>
+          <base-form-textarea
+            v-model="selectedItem.remarks"
+            :can-edit="canEdit"
+            label="備考"
+          />
+          <base-form-text-field
+            v-model="selectedItem.createDate"
+            disabled
+            label="作成日"
+          />
+          <base-form-text-field
+            v-model="selectedItem.updateDate"
+            disabled
+            label="更新日"
+          />
+          <v-btn
+            v-if="!canEdit"
+            class="mb-4 mr-2"
+            color="primary"
+            @click="canEdit = true"
+          >
+            編集する
+          </v-btn>
+          <template
+            v-if="canEdit"
+          >
+            <v-btn
+              class="mb-4 mr-2"
+              color="accent"
+              @click="cancel"
+            >
+              キャンセル
+            </v-btn>
+            <v-btn
+              class="mb-4 mr-2"
+              color="info"
+              :disabled="!valid"
+              @click="save"
+            >
+              更新
+            </v-btn>
+          </template>
+          <v-btn
+            class="mb-4"
+            color="secondary"
+            @click="toList"
+          >
+            一覧画面へ戻る
+          </v-btn>
+        </v-col>
+      </v-row>
     </v-form>
   </v-card>
 </template>
@@ -176,6 +178,7 @@ import { defineComponent, reactive, ref, useRouter } from '@nuxtjs/composition-a
 import { useAccessor } from '~/hooks/useAccessor'
 import type { TableData } from '~/types/data'
 import { required, number } from '~/lib/validation'
+import { VForm } from '~/types/vuetify'
 
 export default defineComponent({
   setup () {
@@ -186,6 +189,9 @@ export default defineComponent({
       number,
       required
     }
+
+    const form = ref<VForm | null>(null)
+    const valid = ref(true)
 
     const canEdit = ref(false)
     const releaseDateMenu = ref(false)
@@ -208,7 +214,14 @@ export default defineComponent({
     })
 
     const save = () => {
-      router.push('/')
+      // 保存処理
+      if (form.value?.validate()) {
+        // 更新日にシステム日付をセット
+        selectedItem.updateDate = new Date().toLocaleString()
+
+        accessor.update(selectedItem)
+        router.push('/')
+      }
     }
 
     const cancel = () => {
@@ -239,12 +252,14 @@ export default defineComponent({
     return {
       cancel,
       canEdit,
+      form,
       purchaseDateMenu,
       releaseDateMenu,
       rules,
       save,
       selectedItem,
-      toList
+      toList,
+      valid
     }
   }
 })
