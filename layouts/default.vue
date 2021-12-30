b4e0de25e655016f4466f2741964dfaeb2abc73b
A.K <ak4.greenworks@gmail.com>
2021-12-30 14:53:38 +0900

    🚧仮置きのログアウトボタン配置

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,12 +9,20 @@
       />
       <v-spacer />
       <v-btn
+        class="mr-4"
         icon
         @click="theme = !theme"
       >
         <v-icon>
           {{ themeIcon }}
         </v-icon>
+      </v-btn>
+      <v-btn
+        color="error"
+        small
+        @click="logout"
+      >
+        ログアウト
       </v-btn>
     </v-app-bar>
     <v-main>
@@ -32,11 +40,12 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, ref, useContext, watch } from '@nuxtjs/composition-api'
+import { computed, defineComponent, ref, useContext, useRouter, watch } from '@nuxtjs/composition-api'
 
 export default defineComponent({
   setup () {
     const { $vuetify } = useContext()
+    const router = useRouter()
 
     const title = 'Mock-App'
     const theme = ref($vuetify.theme.dark)
@@ -45,11 +54,16 @@ export default defineComponent({
       return theme.value ? 'mdi-weather-night' : 'mdi-weather-sunny'
     })
 
+    const logout = () => {
+      router.push('/auth')
+    }
+
     watch(theme, () => {
       $vuetify.theme.dark = theme.value
     })
 
     return {
+      logout,
       theme,
       themeIcon,
       title
