2e872f06684645544ff0b45fc6c8f09bc6d9874c
A.K <ak4.greenworks@gmail.com>
2022-01-01 11:24:46 +0900

    ✨action追加

---
+++ store/actions.ts
@@ -0,0 +1,20 @@
+import { actionTree } from 'typed-vuex'
+import state from './state'
+import type { TableData } from '~/types/data'
+
+export default actionTree(
+  { state },
+  {
+    update ({ commit, state }, field: TableData) {
+      const list: TableData[] = []
+      Object.assign(list, state.itemList)
+
+      const idx = list.findIndex(item => item.id === field.id)
+
+      if (idx >= 0) {
+        list[idx] = field
+        commit('setItemList', list)
+      }
+    }
+  }
+)
--- store/index.ts
+++ store/index.ts
@@ -5,14 +5,15 @@ import { getAccessorType } from 'typed-vuex'
 
 // Keep your existing vanilla Vuex code for state, getters, mutations, actions, plugins, etc.
 import state from './state'
+import actions from './actions'
 import mutations from './mutations'
 
 // This compiles to nothing and only serves to return the correct type of the accessor
 export const accessorType = getAccessorType({
   state,
   // getters,
+  actions,
   mutations
-  // actions,
   // modules: {
   // The key (submodule) needs to match the Nuxt namespace (e.g. ~/store/submodule.ts)
   //   submodule,
