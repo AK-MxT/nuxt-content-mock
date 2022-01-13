9b2d2d393ab68a9361720088550bba96316a050e
A.K <ak4.greenworks@gmail.com>
2022-01-14 01:27:14 +0900

    ✨アラートコンポーネントを定義

---
+++ components/base/Alert.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-alert
+    :value="value"
+    :dismissible="dismissible"
+    :type="type"
+  >
+    {{ message }}
+  </v-alert>
+</template>
+
+<script lang="ts">
+import { defineComponent, watch } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  props: {
+    dismissible: Boolean,
+
+    message: {
+      type: String,
+      required: true
+    },
+
+    type: {
+      type: String,
+      default: 'error'
+    },
+
+    value: Boolean
+  },
+
+  setup (props, { emit }) {
+    watch(() => props.value, (newVal: boolean) => {
+      if (!newVal) { emit('close') }
+    })
+  }
+})
+</script>
