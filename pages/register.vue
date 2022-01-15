1ec9bd8a8425c9ebe334f9d298a3663c4bb4995e
A.K <ak4.greenworks@gmail.com>
2022-01-15 17:46:30 +0900

    ✨書籍登録画面追加

---
+++ pages/register.vue
@@ -0,0 +1,13 @@
+<template>
+  <item-detail
+    register-mode
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  middleware: 'authenticated'
+})
+</script>
