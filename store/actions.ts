d9b8b1e5203d70250e07274d674b200fda33dbe9
A.K <ak4.greenworks@gmail.com>
2022-01-15 17:47:29 +0900

    ♻️書籍登録処理のactionを追加

--- store/actions.ts
+++ store/actions.ts
@@ -5,6 +5,20 @@ import type { TableData } from '~/types/data'
 export default actionTree(
   { state },
   {
+    register ({ commit, state }, field: TableData) {
+      const list: TableData[] = []
+      Object.assign(list, state.itemList)
+
+      // idの最大値+1の値を取得して登録データに付与する
+      const id = String(list.length + 1)
+      field.id = id
+
+      list.push(field)
+
+      commit('setItemList', list)
+      commit('setSnackbar', 'データを登録しました。')
+    },
+
     update ({ commit, state }, field: TableData) {
       const list: TableData[] = []
       Object.assign(list, state.itemList)
